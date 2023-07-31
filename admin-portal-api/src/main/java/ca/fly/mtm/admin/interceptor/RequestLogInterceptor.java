9ec3e64d2b5fdac2a7a56bfefb78eb20e597ad6a
Jinyu Chen <rzco0717@gmail.com>
2023-07-31 12:00:01 -0400

    Disable login feature

--- admin-portal-api/src/main/java/ca/fly/mtm/admin/interceptor/RequestLogInterceptor.java
+++ admin-portal-api/src/main/java/ca/fly/mtm/admin/interceptor/RequestLogInterceptor.java
@@ -1,24 +1,24 @@
-package ca.fly.mtm.admin.interceptor;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-@Slf4j
-@Component
-public class RequestLogInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-
-        log.info("request:" + ((HttpServletRequest)request).getRequestURL());
-
-        return true;
-    }
-}
+//package ca.fly.mtm.admin.interceptor;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.util.Map;
+//import java.util.stream.Collectors;
+//
+//@Slf4j
+//@Component
+//public class RequestLogInterceptor implements HandlerInterceptor {
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+//            throws Exception {
+//
+//        log.info("request:" + ((HttpServletRequest)request).getRequestURL());
+//
+//        return true;
+//    }
+//}
